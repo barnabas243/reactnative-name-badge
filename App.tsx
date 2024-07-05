a250f3593d11280032adfe8b6bd3a0e7ac303481
Tan Tian Fong Barnabas <bttf243@gmail.com>
2024-07-05 18:20:40 +0800

    Update Expo configuration and dependencies, readme and screenshot image

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
-import { LinearGradient } from 'expo-linear-gradient';
+// Author: Barnabas Tan
+// Code was written by Author
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
-
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
 
   return (
     <SafeAreaView style={styles.safeContainer}>
       <StatusBar hidden />
       <LinearGradient
-        colors={['#E6E6FA', '#6305fc']}
+        colors={["#E6E6FA", "#6305fc"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
-        style={styles.container}>
+        style={styles.container}
+      >
         <Text style={styles.welcomeText}>Welcome</Text>
         <Text style={styles.subtitleText}>My name is</Text>
         <View style={styles.boxContainer}>
-          <Text style={styles.innerBoxText}>Barnabas Tan</Text>
+          <Text style={styles.innerBoxText}>Barnabas Tan 😎🥂</Text>
         </View>
       </LinearGradient>
     </SafeAreaView>
@@ -29,44 +32,44 @@ export default function App() {
 const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
-    backgroundColor: '#1E1E1E',
+    backgroundColor: "#1E1E1E",
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 20,
   },
   welcomeText: {
     fontSize: 60,
-    fontFamily: 'Arial',
-    fontWeight: 'bold',
-    color: '#fff',
+    fontFamily: "Arial",
+    fontWeight: "bold",
+    color: "#fff",
     marginBottom: 20,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textTransform: "uppercase",
+    textAlign: "center",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 5,
   },
   subtitleText: {
     fontSize: 24,
-    fontFamily: 'Arial',
-    fontWeight: 'bold',
-    color: '#fff',
+    fontFamily: "Arial",
+    fontWeight: "bold",
+    color: "#fff",
     marginBottom: 20,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textTransform: "uppercase",
+    textAlign: "center",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
     textShadowRadius: 3,
   },
   boxContainer: {
-    width: '100%',
-    height: '20%',
-    backgroundColor: '#fff',
+    width: "100%",
+    height: "30%",
+    backgroundColor: "#fff",
     borderRadius: 15,
-    justifyContent: 'center',
-    shadowColor: '#6305fc',
+    justifyContent: "center",
+    shadowColor: "#6305fc",
     shadowOffset: {
       width: 0.5,
       height: 3,
@@ -74,15 +77,15 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3,
     elevation: 5,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   innerBoxText: {
     fontSize: 60,
-    fontFamily: 'Arial',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#6305fc',
-    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    fontFamily: "Arial",
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#6305fc",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
     textShadowRadius: 1,
   },
 });
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # React Native Badge
-Submission for CM3050 Graded Assignment **1.701 Name badge**.
-This project demonstrates a simple React Native application that displays a badge with a gradient background and shadow effects.
+
+Submission for **CM3050 Graded Assignment 1.701 Name badge**.
+
+This project demonstrates a minimalist React Native application featuring a name badge with a gradient background and shadow effects.
+
+please [Click here](https://snack.expo.dev/@barnabas243/reactnative-name-badge) or view the expo.txt file for the link to the expo snack demo.
 
 ## Table of Contents
+
 - [Preview](#preview)
 - [Prerequisites](#prerequisites)
 - [Installation](#installation)
@@ -10,6 +15,7 @@ This project demonstrates a simple React Native application that displays a badg
 - [License](#license)
 
 ## Preview:
+
 ![Badge preview](screenshot.jpg)
 
 ## Prerequisites
@@ -20,7 +26,9 @@ Before running the app, make sure you have Node.js and Expo CLI installed on you
 - [Expo CLI](https://docs.expo.dev/get-started/installation/)
 
 ## Installation
+
 1. **Clone the repository:**
+
    ```bash
    git clone https://github.com/barnabas243/reactnative-name-badge.git
    cd reactnative-name-badge
@@ -35,15 +43,18 @@ Before running the app, make sure you have Node.js and Expo CLI installed on you
 
 To start the app locally, use the following command:
 
-   ```bash
-   npm start
-   ```
-   or
-   ```bash
-   npx expo start
-   ```
+```bash
+npm start
+```
+
+or
+
+```bash
+npx expo start
+```
 
 This will open the Expo development server and provide you with options to run the app on an Android/iOS simulator or scan the QR code with the Expo Go app on your device.
 
 ## License
+
 This project is licensed under the [MIT License](LICENSE).
